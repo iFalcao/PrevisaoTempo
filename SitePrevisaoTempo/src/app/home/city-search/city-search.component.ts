c93f53616940ac3049219255ae383174bdff447c
iFalcao <vbsfalcao@gmail.com>
2019-05-25 14:53:33 -0300

    City search component communicates with API to return data + present it as a placeholder

--- SitePrevisaoTempo/src/app/home/city-search/city-search.component.ts
+++ SitePrevisaoTempo/src/app/home/city-search/city-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { City } from 'src/app/_models/city';
+import { WeatherService } from 'src/app/_services/weather/weather.service';
+import { AlertifyService } from 'src/app/_services/alertify/alertify.service';
 
 @Component({
   selector: 'app-city-search',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city-search.component.css']
 })
 export class CitySearchComponent implements OnInit {
+  foundCities: any = [];
+  model: any = {};
+  pesquisaFeita = false;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
+  searchCities(): void {
+    if (this.model.cityName === undefined
+      || this.model.cityName.length === 0) {
+      this.alertify.error('Por favor, informe o nome da cidade que deseja buscar.');
+      return;
+    }
+    this.weatherService.searchCities(this.model.cityName)
+      .subscribe((cities: any) => {
+        console.log(cities.list);
+        this.foundCities = cities.list;
+      }, error => {
+        console.log(error);
+        this.alertify.error(error);
+      }, () => {
+        this.pesquisaFeita = true;
+      });
+  }
 }
