d7f9a1ec1947657b0f3bef791ef40f7d5181b40c
iFalcao <vbsfalcao@gmail.com>
2019-05-24 22:08:17 -0300

    Configure sqlite as the project's database and generate first migration + ignore database file

---
+++ APIPrevisaoTempo/WebApi/APIPrevisaoTempo.WebApi/Migrations/20190525010432_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIPrevisaoTempo.WebApi.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    CustomCode = table.Column<string>(maxLength: 30, nullable: false),
+                    Latitude = table.Column<double>(type: "decimal(9,6)", nullable: true),
+                    Longitude = table.Column<double>(type: "decimal(9,6)", nullable: true),
+                    Country = table.Column<string>(maxLength: 3, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
