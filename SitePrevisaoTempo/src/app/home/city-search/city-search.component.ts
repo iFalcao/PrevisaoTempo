d83205497e040483efc5a74cf7ec0bddfdbf7728
iFalcao <vbsfalcao@gmail.com>
2019-05-26 00:37:50 -0300

    Add styles to city search and city list components

--- SitePrevisaoTempo/src/app/home/city-search/city-search.component.ts
+++ SitePrevisaoTempo/src/app/home/city-search/city-search.component.ts
@@ -7,40 +7,50 @@ import { CityService } from 'src/app/_services/city/city.service';
 @Component({
   selector: 'app-city-search',
   templateUrl: './city-search.component.html',
-  styleUrls: ['./city-search.component.css']
+  styleUrls: ['./city-search.component.scss']
 })
 export class CitySearchComponent implements OnInit {
   foundCities: City[] = [];
   model: any = {};
   pesquisaFeita = false;
 
-  constructor(private weatherService: WeatherService, private alertify: AlertifyService, private cityService: CityService) { }
+  constructor(
+    private weatherService: WeatherService,
+    private alertify: AlertifyService,
+    private cityService: CityService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   searchCities(): void {
-    if (this.model.cityName === undefined
-      || this.model.cityName.length === 0) {
-      this.alertify.error('Por favor, informe o nome da cidade que deseja buscar.');
+    if (this.model.cityName === undefined || this.model.cityName.length === 0) {
+      this.alertify.error(
+        'Por favor, informe o nome da cidade que deseja buscar.'
+      );
       return;
     }
-    this.weatherService.searchCities(this.model.cityName)
-      .subscribe((cities: City[]) => {
+    this.weatherService.searchCities(this.model.cityName).subscribe(
+      (cities: City[]) => {
         this.foundCities = cities;
-      }, error => {
+      },
+      error => {
         this.alertify.error(error);
-      }, () => {
+      },
+      () => {
         this.pesquisaFeita = true;
-      });
+      }
+    );
   }
 
   saveCity(newCity: City): void {
-    this.cityService.insertCity(newCity).subscribe((insertedCity: City) => {
-      this.alertify.success('Cidade cadastrada com sucesso.');
-      this.cityService.noticeCityInsertion(insertedCity);
-    }, error => {
-      this.alertify.error(error);
-    });
+    this.cityService.insertCity(newCity).subscribe(
+      (insertedCity: City) => {
+        this.alertify.success('Cidade cadastrada com sucesso.');
+        this.cityService.noticeCityInsertion(insertedCity);
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 }
