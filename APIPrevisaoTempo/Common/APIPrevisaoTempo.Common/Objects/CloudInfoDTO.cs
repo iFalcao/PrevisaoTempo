768e4a02e8aa68d9559e8c37f2c6f662440bafff
iFalcao <vbsfalcao@gmail.com>
2019-05-27 00:12:55 -0300

    Rename service solution folder to application and remove unused projects

--- APIPrevisaoTempo/Common/APIPrevisaoTempo.Common/Objects/CloudInfoDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace APIPrevisaoTempo.Common.Objects
-{
-    public class CloudInfoDTO
-    {
-        public int all { get; set; }
-    }
-}
--- APIPrevisaoTempo/Common/APIPrevisaoTempo.Common/Objects/WindDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace APIPrevisaoTempo.Common.Objects
-{
-    public class WindDTO
-    {
-        public double speed { get; set; }
-        public double deg { get; set; }
-    }
-}
